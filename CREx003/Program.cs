5dfadb3d2d0b0ee50ef9b109b848330d018ce9d9
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-02-27 22:20:26 -0300

    Feat: Correção exercício 3 no Console

---
+++ CREx003/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CREx003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int maior = Math.Max(num1, num2);
+
+            Console.WriteLine($"O número maior é: {maior}");
+        }
+    }
+}
