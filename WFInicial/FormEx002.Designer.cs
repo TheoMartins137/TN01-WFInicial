d417cf261c4257e4627e7ffd313d74b75ffadaa6
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-02-27 22:02:56 -0300

    feat: Exercício 2 no Windows Forms

--- WFInicial/FormEx002.Designer.cs
+++ WFInicial/FormEx002.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormEx002";
+            lbl_diasemana = new Label();
+            txt_diaSemana = new TextBox();
+            btn_escolher = new Button();
+            SuspendLayout();
+            // 
+            // lbl_diasemana
+            // 
+            lbl_diasemana.AutoSize = true;
+            lbl_diasemana.Location = new Point(82, 47);
+            lbl_diasemana.Name = "lbl_diasemana";
+            lbl_diasemana.Size = new Size(88, 15);
+            lbl_diasemana.TabIndex = 0;
+            lbl_diasemana.Text = "Dia da Semana:";
+            lbl_diasemana.Click += label1_Click;
+            // 
+            // txt_diaSemana
+            // 
+            txt_diaSemana.Location = new Point(176, 43);
+            txt_diaSemana.Name = "txt_diaSemana";
+            txt_diaSemana.Size = new Size(96, 23);
+            txt_diaSemana.TabIndex = 1;
+            // 
+            // btn_escolher
+            // 
+            btn_escolher.Location = new Point(278, 43);
+            btn_escolher.Name = "btn_escolher";
+            btn_escolher.Size = new Size(75, 23);
+            btn_escolher.TabIndex = 2;
+            btn_escolher.Text = "Confirmar";
+            btn_escolher.UseVisualStyleBackColor = true;
+            btn_escolher.Click += btn_escolher_Click;
+            // 
+            // FormEx002
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(424, 167);
+            Controls.Add(btn_escolher);
+            Controls.Add(txt_diaSemana);
+            Controls.Add(lbl_diasemana);
+            Name = "FormEx002";
+            Text = "FormEx002";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbl_diasemana;
+        private TextBox txt_diaSemana;
+        private Button btn_escolher;
     }
 }
--- WFInicial/FormEx002.cs
+++ WFInicial/FormEx002.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WFInicial
 {
@@ -15,6 +16,44 @@ namespace WFInicial
         public FormEx002()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_escolher_Click(object sender, EventArgs e)
+        {
+            byte opcao = byte.Parse(txt_diaSemana.Text);
+
+            switch (opcao)
+            {
+                case 1:
+                    MessageBox.Show("Domingo");
+                    break;
+                case 2:
+                    MessageBox.Show("Segunda-feira");
+                    break;
+                case 3:
+                    MessageBox.Show("Terça-feira");
+                    break;
+                case 4:
+                    MessageBox.Show("Quarta-feira");
+                    break;
+                case 5:
+                    MessageBox.Show("Quinta-feira");
+                    break;
+                case 6:
+                    MessageBox.Show("Sexta-feira");
+                    break;
+                case 7:
+                    MessageBox.Show("Sábado");
+                    break;
+                default:
+                    MessageBox.Show("Inválido");
+                    break;
+            }
         }
     }
 }
