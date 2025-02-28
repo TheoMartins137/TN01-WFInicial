72cb763369a6341d47648a0161d914a928159d3d
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-02-28 19:17:59 -0300

    feat: Exercicio 4 corrigido no Console

---
+++ CREx004/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CREx004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o terceiro número");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int maior = Math.Max(num3, Math.Max(num1, num2));
+
+            Console.WriteLine($"O número maior é: {maior}");
+        }
+    }
+}
