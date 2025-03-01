bbb7152ab6c1d6de77abad50f00b1d618dd73141
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-02-28 22:19:50 -0300

    fix: algo

--- WFInicial/FormEx007.Designer.cs
+++ WFInicial/FormEx007.Designer.cs
@@ -37,7 +37,7 @@
             // 
             // btn_confirm
             // 
-            btn_confirm.Location = new Point(146, 111);
+            btn_confirm.Location = new Point(123, 113);
             btn_confirm.Name = "btn_confirm";
             btn_confirm.Size = new Size(100, 23);
             btn_confirm.TabIndex = 0;
@@ -47,7 +47,7 @@
             // lbl_nome
             // 
             lbl_nome.AutoSize = true;
-            lbl_nome.Location = new Point(79, 42);
+            lbl_nome.Location = new Point(56, 44);
             lbl_nome.Name = "lbl_nome";
             lbl_nome.Size = new Size(50, 15);
             lbl_nome.TabIndex = 1;
@@ -55,14 +55,14 @@
             // 
             // txt_nome
             // 
-            txt_nome.Location = new Point(146, 72);
+            txt_nome.Location = new Point(123, 74);
             txt_nome.Name = "txt_nome";
             txt_nome.Size = new Size(100, 23);
             txt_nome.TabIndex = 2;
             // 
             // txt_valor
             // 
-            txt_valor.Location = new Point(146, 39);
+            txt_valor.Location = new Point(123, 41);
             txt_valor.Name = "txt_valor";
             txt_valor.Size = new Size(100, 23);
             txt_valor.TabIndex = 3;
@@ -70,7 +70,7 @@
             // lbl_valor
             // 
             lbl_valor.AutoSize = true;
-            lbl_valor.Location = new Point(79, 75);
+            lbl_valor.Location = new Point(56, 77);
             lbl_valor.Name = "lbl_valor";
             lbl_valor.Size = new Size(33, 15);
             lbl_valor.TabIndex = 4;
@@ -80,7 +80,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(321, 273);
             Controls.Add(lbl_valor);
             Controls.Add(txt_valor);
             Controls.Add(txt_nome);
